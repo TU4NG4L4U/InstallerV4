2278d2351e360d6e44e43e1381f4885d17b24905
TU4NG4L4U <46773642+TU4NG4L4U@users.noreply.github.com>
2019-01-31 20:11:32 +0700

    Add files via upload

---
+++ TuAnG4lAu.sh
@@ -0,0 +1,150 @@
+clear
+sleep 2
+echo "\033[32;1m"
+echo "Tools Yg Sering Dipakai..... Sabar Kamvank.....!!!"
+sleep 3
+clear
+echo ""
+echo 
+"\033[34;1m{$}\033[37;1m=================================\033[34;1m{$}"
+echo ""
+echo "\033[32;1m   ~\033[32;1m  Author    : TU4N G4L4U"
+echo "\033[32;1m   ~\033[32;1m  Version   : 1.0"
+echo "\033[32;1m   ~\033[32;1m  Team      : WOLF CYBER 
+ARMY"
+echo "\033[32;1m   ~\033[32;1m  Feat      : Sk4ters"
+echo ""
+echo 
+"\033[34;1m{$}\033[37;1m=================================\033[34;1m{$}"
+echo "\033[34;1m  1 
+\033[37;1m.\033[32;1mLITEDDOS\033[37;1m"
+echo "\033[34;1m  2 \033[37;1m.\033[32;1mSpider Bot 
+\033[37;1m"
+echo "\033[34;1m  3 
+\033[37;1m.\033[32;1mLITETOOLS\033[37;1m"
+echo "\033[34;1m  4 
+\033[37;1m.\033[32;1mLITESCRIPT\033[37;1m"
+echo "\033[34;1m  5 \033[37;1m.\033[32;1mLazymux\033[37;1m"
+echo "\033[34;1m  6 \033[37;1m.\033[32;1mKo-Dork 
+(ScanWeb)\033[37;1m"
+echo "\033[34;1m  7 \033[37;1m.\033[32;1mSpazSms\033[37;1m"
+echo "\033[34;1m  8 
+\033[37;1m.\033[32;1mMetasploit\033[37;1m"
+echo "\033[34;1m  9 
+\033[37;1m.\033[32;1mHash-Buster\033[37;1m"
+echo "\033[34;1m  10\033[37;1m.\033[32;1mSqlMAP\033[37;1m"
+echo "\033[34;1m  11\033[37;1m.\033[32;1mBot Comment 
+Fb\033[37;1m"
+echo "\033[34;1m  
+12\033[37;1m.\033[32;1mTorshammer\033[37;1m"
+echo "\033[34;1m  
+13\033[37;1m.\033[32;1mIPGeolocation\033[37;1m"
+echo "\033[34;1m  
+14\033[37;1m.\033[32;1mRecondog\033[37;1m"
+echo "\033[34;1m  15\033[37;1m.\033[32;1mCrewbot\033[37;1m"
+echo "\033[34;1m  
+16\033[37;1m.\033[32;1miesDeface\033[37;1m"
+echo "\033[34;1m  
+17\033[37;1m.\033[32;1mRed_Hawk\033[37;1m"
+echo "\033[34;1m  18\033[37;1m.\033[32;1mWeeman\033[37;1m"
+echo "\033[34;1m  19\033[37;1m.\033[32;1mD-TECT\033[37;1m"
+echo "\033[34;1m  20\033[37;1m.\033[32;1mHammer\033[37;1m"
+echo "\033[34;1m  
+21\033[37;1m.\033[32;1mKatoolin\033[37;1m"
+echo "\033[34;1m  22\033[37;1m.\033[32;1mTelp Pakai 
+Termux\033[37;1m"
+echo "\033[34;1m  23\033[37;1m.\033[32;1mMembuat Tulisan 
+Figlet\033[37;1m"
+echo "\033[34;1m  24\033[37;1m.\033[32;1mXerxes\033[37;1m"
+echo "\033[34;1m  25\033[37;1m.\033[32;1mMain 
+Moon-Buggy\033[37;1m"
+echo "\033[34;1m  26\033[37;1m.\033[32;1mTx-Tools 
+\033[37;1m"
+echo "\033[34;1m  27\033[37;1m.\033[32;1mviSQL\033[37;1m"
+echo "\033[34;1m  
+28\033[37;1m.\033[32;1mIESinstaller\033[37;1m"
+echo "\033[34;1m  29\033[37;1m.\033[32;1mSocialFish 
+(Phising)\033[37;1m"
+echo "\033[34;1m  30\033[37;1m.\033[32;1mBinGoo 
+(Dorking)\033[37;1m"
+echo "\033[34;1m  31\033[37;1m.\033[32;1mCupp 
+(Wordlist)\033[37;1m"
+echo "\033[34;1m  32\033[37;1m.\033[32;1mA-Rat \033[37;1m"
+echo "\033[34;1m  33\033[37;1m.\033[32;1mPhising Mania 
+(Phising)\033[37;1m"
+
+echo ""
+echo ""
+echo "\033[34;1m  99\033[37;1m.\033[32;1mKeluar\033[37;1m"
+echo ""
+echo "\033[32;1m ──> \033[32;1m Pilih pilih aja gk usah 
+komen!\033[32;1m"
+read -p " TU4N G4L4U " pilih
+if [ $pilih = 1 ]
+then
+clear
+toilet -f slant -F gay "TU4N G4L4U" | lolcat
+echo "\033[34;1mLoading.....!!!"
+echo ""
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+toilet -f slant -F gay "TU4N G4L4U" | lolcat
+echo "\033[34;1mLoading.....!!!"
+echo ""
+sleep 1
+apt update && apt upgrade
+apt install php
+apt install git
+git clone https://github.com/Cvar1984/SpiderBot.git
+cd SpiderBot
+echo ""
+echo "\033[36;1m"
+echo "  Tinggal DiJalankan Dech:)"
+echo "  Contoh Saya Ingin Pakai Botkomen"
+echo "  Ketik: php botkoment.php"
+echo ""
+ls
+fi
+
+if [ $pilih = 3 ]
+then
+clear
+toilet -f slant -F gay "TU4N G4L4U" | lolcat
+echo "\033[34;1mLoading.....!!!"
+echo ""
+sleep 1
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/4L13199/LITETOOLS
+cd LITETOOLS
+sh LITETOOLS.isl
+fi
+
+if [ $pilih = 4 ]
+then
+clear
+toilet -f slant -F gay "TU4N G4L4U" | lolcat
+echo "\033[34;1mLoading.....!!!"
+echo ""
+sleep 1
+pkg install python2 git
+git clone https://github.com/4L13199/LITESCRIPT
+echo ""
+echo "  Buat Simpen Ke Internal"
+echo "  mv namascript.html /sdcard/"
+echo ""
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if 
